07b19b6e3ae243e31b6a1992ddd82b5042e0b487
Whitney Seiler <whitney.seiler@gmail.com>
2018-05-02 12:46:38 -0700

    Refactor request handler to pass linter

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,5 +1,6 @@
 /* Import node's http module: */
 var http = require('http');
+var handleRequest = require('./request-handler.js');
 
 
 // Every server needs to listen on a port with a unique number. The
@@ -15,22 +16,12 @@ var port = 3000;
 var ip = '127.0.0.1';
 
 
-
-// We use node's http module to create a server.
-//
-// The function we pass to http.createServer will be used to handle all
-// incoming requests.
-//
-// After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+// We use node's http module to create a server. The function we pass to http.createServer will be used to handle all incoming requests. After creating the server, we will tell it to listen on the given port and IP. */
+var server = http.createServer(handleRequest.requestHandler);
 console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
+// To start this server, run: node basic-server.js on the command line.
 //
 // To connect to the server, load http://127.0.0.1:3000 in your web
 // browser.
@@ -38,4 +29,3 @@ server.listen(port, ip);
 // server.listen() will continue running as long as there is the
 // possibility of serving more requests. To stop your server, hit
 // Ctrl-C on the command line.
-
--- server/request-handler.js
+++ server/request-handler.js
@@ -11,63 +11,73 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+var sendResponse = function(response, data, statuscode) {
+  statuscode = statuscode || 200;
+  response.writeHead(statuscode, headers);
+  response.end(JSON.stringify(data));
+};
 
-  // Do some basic logging.
+var collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(JSON.parse(data));
+  });
+};
+
+var messages = [
+  { text: 'hello world',
+    username: 'Whitney'
+  }
+];
+
+var actions = {
+  'GET': function(request, response) {
+    sendResponse(response, {results: messages});
+  },
+  'POST': function(request, response) {
+    collectData(request, function(message) {
+      message;
+      sendResponse(response, 'Ahoy!');
+    });
+  },
+  'OPTIONS': function(request, response) {
+    sendResponse(response, 'Ahoy!');
+  }
+};
+
+module.exports = function(request, response) {
+  // Request and Response come from node's http module. They include information about both the incoming request, such as headers and URL, and about the outgoing response, such as its status and content.
   //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+  // Documentation for both request and response can be found in the HTTP section at http://nodejs.org/documentation/api/ Do some basic logging.
+
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  var action = actions[request.method];
+  if (action) {
+    action(request, response);
+  } else {
+    //TODO error handling
+  }
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
 //
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
+// Your chat client is running from a url like file://your/chat/client/index.html, which is considered a different domain.
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
+// exports.requestHandler = requestHandler;
